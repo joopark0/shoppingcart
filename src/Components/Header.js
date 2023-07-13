1b6097e74a218d97ac12537c72ea42f9e06f1708
errMeep <andypark6004@gmail.com>
2023-07-13 16:51:51 -0400

    Adding logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import shoppingcart from '../images/shopping-cart.png';
+import logo from '../images/logo-no-background.png';
 
 const Header = () => {
   return (
     <div>
       <div className="header-box">
-        <nav className="logo-top">LOGO</nav>
+        <button className="nav-logo"></button>
         <nav className="nav-bar">
           <button className="nav-buttons">Home</button>
           <button className="nav-buttons">Store</button>
