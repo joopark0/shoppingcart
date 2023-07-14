a7ba6d7cb9716c1f0b91b113b863be5c02ae4323
errMeep <andypark6004@gmail.com>
2023-07-13 22:14:01 -0400

    Restructure Header and adding routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
-import Header from './components/Header';
+import shoppingcart from './images/shopping-cart.png';
 import HomePage from './components/HomePage';
+import Store from './components/Store';
 import Footer from './components/Footer';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
-      <HomePage />
+      <div>
+        <div className="header-box">
+          <button className="nav-logo"></button>
+          <div className="nav-right-container">
+            <nav className="nav-bar">
+              <Link to="/" className="nav-buttons">
+                <button className="nav-buttons">Home</button>
+              </Link>
+              <Link to="/Store" className="nav-buttons">
+                <button className="nav-buttons">Store</button>
+              </Link>
+              <button className="nav-buttons">About Us</button>
+              <button className="nav-buttons">Contact Us</button>
+            </nav>
+            <button className="shoppingcart">
+              <img
+                src={shoppingcart}
+                alt="Shopping Cart"
+                className="shoppingcartimg"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/Store" element={<Store />}></Route>
+      </Routes>
       <Footer />
     </div>
   );
