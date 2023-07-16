928e8339c67faf6c617474d9f367afa80bf735b4
errMeep <andypark6004@gmail.com>
2023-07-15 20:10:38 -0400

    Adding layout for store container

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -1,5 +1,10 @@
 const Store = () => {
-  return <div></div>;
+  return (
+    <div className="store-container">
+      <div className="menu-section-container"></div>
+      <div className="item-section-container"></div>
+    </div>
+  );
 };
 
 export default Store;
