97855a35a529fc06904165b3f9b7b4b3ef3874a1
errMeep <andypark6004@gmail.com>
2023-07-15 21:16:14 -0400

    Adding item layout

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -1,3 +1,5 @@
+import gr86 from '../images/gr86model.jpeg';
+
 const Store = () => {
   return (
     <div className="store-container">
@@ -8,9 +10,21 @@ const Store = () => {
           <div className="menu-category">Accessory</div>
           <div className="menu-category">Models</div>
         </div>
-        <div className="menu-whitespace"></div>
+        <div className="menu-whitespace-bottom"></div>
       </div>
-      <div className="item-section-container"></div>
+      <div className="item-section-container">
+        <div className="item-top-container">
+          <div className="item-image-container">
+            <img src={gr86} alt="GR86" />
+          </div>
+          <div className="item-info-container">
+            <div className="item-name">
+              <h1>GR86 Model</h1>
+            </div>
+          </div>
+        </div>
+        <div className="item-bottom-container"></div>
+      </div>
     </div>
   );
 };
