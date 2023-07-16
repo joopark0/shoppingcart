cb88e1d34af51b7a074e6345b655e1a89b9d33f3
errMeep <andypark6004@gmail.com>
2023-07-15 20:28:14 -0400

    Adding layout of menu section of store

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -1,7 +1,13 @@
 const Store = () => {
   return (
     <div className="store-container">
-      <div className="menu-section-container"></div>
+      <div className="menu-section-container">
+        <div className="menu-category-container">
+          <div className="menu-category">Clothing</div>
+          <div className="menu-category">Accessory</div>
+          <div className="menu-category">Models</div>
+        </div>
+      </div>
       <div className="item-section-container"></div>
     </div>
   );
