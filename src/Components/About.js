347988465a4bc10e322988af929c827d12077df9
errMeep <andypark6004@gmail.com>
2023-07-20 15:53:53 -0400

    banner about image adding

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,12 @@
 import brzimage from '../images/varisbrz.jpeg';
+import brzbanner from '../images/brzbanner.jpeg';
 const About = () => {
   return (
     <div className="about-container">
-      <div className="about-whitespace"></div>
-      <div className="abount-wrap">
+      <div className="about-whitespace">
+        <img src={brzbanner} alt="brztrackparts" />
+      </div>
+      <div className="about-wrap">
         <img src={brzimage} alt="brzvaris" />
         <div className="about-desc">test text</div>
       </div>
